f89c8bc8bc7acffcc15393b75a5c937d6dddbddc
Katie Millar <ajh20fhu@uea.ac.uk>
2023-03-06 16:54:38 +0000

    Adding virgin female diet preference to the simulation.

--- preferred-diet-choice.R
+++ preferred-diet-choice.R
@@ -3,10 +3,16 @@
 #  A simulation which includes 4 protein: carbohydrate diets and a no diet option 
 
 
+
+
+#-------------- mated females
+
+
 # Reading the different diets in 
 diets <- c("8:1", "2:1", "1:8", "1:2", "nodiet")
 
 # The mean average of each of the diets from a previous experiment 
+
 meanpreference <- c(2.74, 1.73, 1.36, 0.98, 3.19)
 
 # the amount of replicate feeding assays 
@@ -46,6 +52,48 @@ cat("Preferred diet:", preferred_diet, "\n")
 
 mean_dietpreference
 
+#-------------- virgin females
+
+# Reading the different diets in 
+dietsv <- c("8:1", "2:1", "1:8", "1:2", "nodiet")
+
+# The mean average of each of the diets from a previous experiment 
+meanpreferencev <- c(1.64, 1.81, 2.55, 1.46, 2.54)
+
+# the amount of replicate feeding assays 
+replicatesv <- 10
+
+# the number of flies in each feeding assay 
+fliesv <- 10
+
+# putting the data into columns 
+dietpreferencev <- matrix(0, nrow = replicatesv, ncol = length(dietsv), dimnames = list(NULL, dietsv))
+
+# Loop over each replicate
+for (i in 1:replicatesv) {   
+  choicesv <- sample(dietsv, size = fliesv, replace = TRUE, prob = meanpreferencev)
+  for (j in 1:length(dietsv)) {
+    dietpreferencev[i, j] <- sum(choicesv == dietsv[j])
+  }
+}
 
 
+# Calculate the mean number of flies on each diet patch across all replicates
+mean_dietpreferencev <- colMeans(dietpreferencev)
+
+# Identify the preferred diet
+preferred_dietv <- dietsv[which.max(mean_dietpreferencev)]
+
+
+
+
+
+# Print the results
+cat("Mean number of flies on each diet patch:", mean_dietpreferencev, "\n")
+cat("Preferred diet:", preferred_dietv, "\n")
+
+#  cat converts its arguments to character strings, concatenates them, separating
+#  them by the given sep= string, and then prints them
+
+mean_dietpreferencev
 

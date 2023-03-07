3e0d5021342b5e044e519683d64e280943ceb1ef
Katie Millar <ajh20fhu@uea.ac.uk>
2023-03-07 20:46:23 +0000

    Creating a proportion value simulation

--- scripts/preferred-diet-choice.R
+++ scripts/preferred-diet-choice.R
@@ -14,24 +14,22 @@ diets <- c("8:1", "2:1", "1:8", "1:2", "nodiet")
 # The mean average of each of the diets from a previous experiment 
 meanpreference <- c(2.74, 1.73, 1.36, 0.98, 3.19)
 
-# the amount of replicate feeding assays? or the amount of times to run the simulation
+# the amount of replicate feeding assays? or the amount of times to run the simulation?? 
 replicates <- 10
-
 # the number of flies in each feeding assay 
 flies <- 10
 
 # putting the data into a 'matrix' with amount of times to repeat, diet names to include 
 dietpreference <- matrix(0, nrow = replicates, ncol = length(diets), dimnames = list(NULL, diets))
 
+
 # Loop over each replicate
-for (i in 1:replicates) {
-  
+
+ for (i in 1:replicates) {choices <- sample(diets, size = flies, replace = TRUE, prob = meanpreference)
+  for (j in 0:length(diets)) {
+    dietpreference[i, j] <- sum(choices == diets[j])}}
+
 # the simulation including diet names, overall amount of flies with the known mean preferences of a fly on each patch
-  choices <- sample(diets, size = flies, replace = TRUE, prob = meanpreference)
-  for (j in 1:length(diets)) {
-    dietpreference[i, j] <- sum(choices == diets[j])
-  }
-}
 
 # using the matrix to find the the mean diet preference of the replicates (10 or 100) using colMeans
 mean_dietpreference <- colMeans(dietpreference)
@@ -49,11 +47,14 @@ cat("Preferred diet:", preferred_diet, "\n")
 
 # simulation for a mean diet preference of each diet 
 mean_dietpreference
-
+#------ proportion results for one fly? 
 #  trying to repeat simulation 100 times 
-results <- replicate(1000, choices())
+proportionresults <- replicate(1000, choices)
 
-prop.results <- prop.table(table(results))
+# diet prportion 
+prop.results <- prop.table(table(proportionresults))
+
+prop.results
 
 #-------------- Virgin females ------------
 
@@ -72,13 +73,9 @@ fliesv <- 10
 # putting the data into columns 
 dietpreferencev <- matrix(0, nrow = replicatesv, ncol = length(dietsv), dimnames = list(NULL, dietsv))
 
-# Loop over each replicate
-for (i in 1:replicatesv) {   
-  choicesv <- sample(dietsv, size = fliesv, replace = TRUE, prob = meanpreferencev)
-  for (j in 1:length(dietsv)) {
-    dietpreferencev[i, j] <- sum(choicesv == dietsv[j])
-  }
-}
+
+for (i in 1:replicatesv) {choicesv <- sample(dietsv, size = fliesv, replace = TRUE, prob = meanpreferencev)
+  for (j in 1:length(dietsv)) {dietpreferencev[i, j] <- sum(choicesv == dietsv[j])}}
 
 
 # Calculate the mean number of flies on each diet patch across all replicates
@@ -88,10 +85,6 @@ mean_dietpreferencev <- colMeans(dietpreferencev)
 preferred_dietv <- dietsv[which.max(mean_dietpreferencev)]
 
 
-
-
-
-# Print the results
 cat("Mean number of flies on each diet patch:", mean_dietpreferencev, "\n")
 cat("Preferred diet:", preferred_dietv, "\n")
 
